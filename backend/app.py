6f01b3d245c99060bfbe009fae21bd4168f88c62
Sumona17 <sumona.mondal@exavalu.com>
2025-05-06 18:33:14 +0530

    Version1.0.6

--- backend/app.py
+++ backend/app.py
@@ -79,5 +79,5 @@ async def upload_file(file: UploadFile = File(...)):
     return {"response": response}
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
  
